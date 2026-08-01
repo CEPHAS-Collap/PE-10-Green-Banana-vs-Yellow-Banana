40b39c3bce7365b8511ccc1902244846eda1c579
Akan-7 <etimbukakan@gmail.com>
2026-08-02 00:16:48 +0100

    loading and defining configurations

--- app.py
+++ app.py
@@ -21,26 +21,3 @@ model = load_model()
 # ------------------------------------
 # Prediction Function
 # ------------------------------------
-def predict(image):
-
-    image = image.convert("RGB")
-    image = image.resize(IMG_SIZE)
-
-    img = np.array(image).astype(np.float32)
-
-    img = tf.keras.applications.mobilenet_v2.preprocess_input(img)
-
-    img = np.expand_dims(img, axis=0)
-
-    prediction = model.predict(img, verbose=0)[0][0]
-
-    if prediction >= 0.5:
-        label = " Yellow (Ripe)"
-        confidence = prediction
-    else:
-        label = " Green (Unripe)"
-        confidence = 1 - prediction
-
-    return label, confidence
-
-
